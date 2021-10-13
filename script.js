46e6bb4d67b7a75a7ef9e72c3acc8eaa5963eba4
Rakibul-98 <rakibul.rupom2001@gmail.com>
2021-10-13 21:35:18 +0600

    pin matching and display alert message

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function generatePin(){
+function handleGenerate(){
     const randomNumber = Math.floor(Math.random()*10000);
     if (randomNumber > 1000) {
         document.getElementById("generate-display").value=randomNumber;
     }
+}
+
+function handleTypedPin(){
+    const digit = event.target.innerText;
+    if (isNaN(digit)) {
+        if(digit == 'C'){
+            document.getElementById("typed-display").value='';
+        }
+    }
+    else{
+        const pin = document.getElementById("typed-display").value;
+        const newPin = pin+digit;
+        document.getElementById("typed-display").value=newPin;
+    }
+}
+function handleSubmit() {
+    const generatedPin = document.getElementById("generate-display").value;
+    const typedPin = document.getElementById("typed-display").value;
+    if(generatedPin == typedPin){
+        document.getElementById("pin-matched").style.display='block';
+        document.getElementById("pin-not-matched").style.display='none';
+    }
+    else{
+        document.getElementById("pin-matched").style.display='none';
+        document.getElementById("pin-not-matched").style.display='block';
+    }
+    document.getElementById("generate-display").value='';
+    document.getElementById("typed-display").value='';
 }
